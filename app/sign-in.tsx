7e82b1e633f7156c94369dd333d18815804bc5de
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-01-13 11:27:17 -0500

    Add files via upload

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,20 @@
-import {View, Text, ScrollView, Image, TouchableOpacity} from 'react-native';
+import {View, Text, ScrollView, Image, TouchableOpacity, Alert} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import images from '@/constants/images';
 import icons from '@/constants/icons';
+import {login} from "@/lib/appwrite";
 
 const SignIn = () => {
-    const handleLogin = () => {};
+    const handleLogin = async() => {
+        const result = await login();
+
+        if(result) {
+            console.log('Login Success✅');
+        } else {
+            Alert.alert('Error', 'Failed to Login🚫')
+        }
+    };
     return (
         <SafeAreaView className="bg-white h-full">
             <ScrollView contentContainerClassName="h-full">
@@ -13,7 +22,7 @@ const SignIn = () => {
 
                 <View className="px-10">
                     <Text className="text-base text-center uppercase font-rubik text-black-200">
-                        Welcome to SaturnBay Estates!
+                        Welcome to SaturnBay Estates 👋
                     </Text>
 
                     <Text className="text-3xl text-center font-rubik-bold text-black-300 mt-2">
