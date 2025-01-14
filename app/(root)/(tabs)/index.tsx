38e714235d9d6d36b0155fffaf8351da6d89b90a
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-01-14 13:06:11 -0500

    Add files via upload

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,28 @@
-import { Text, View } from "react-native";
+import {Image, Text, View} from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {useGlobalContext} from "@/lib/global-provider";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+
 
 export default function Index() {
+    const { user } = useGlobalContext();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <Text className="font-bold my-10 font-rubik text-3xl">Welcome to SaturnBay Estates</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+        <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+                <View className="flex flex-row items-center">
+                    <Image source={images.Cortez}
+                    className="size-12 rounded-full"/>
+                    <View className="flex flex-col items-start ml-2 justify-center">
+                        <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                        <Text className="text-base font-rubik-medium text-black-300">Zach Cortez</Text>
+                    </View>
+                </View>
+                <Image source={icons.bell} className="size-6"/>
+            </View>
+        </View>
+    </SafeAreaView>
   );
 }
