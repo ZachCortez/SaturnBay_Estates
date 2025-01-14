dc11a487d9193740fcf3e4428e822462215dcc3c
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-01-14 15:30:10 -0500

    Add files via upload

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {Image, Text, View} from "react-native";
+import {Image, Text, View, TouchableOpacity} from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {useGlobalContext} from "@/lib/global-provider";
@@ -23,8 +23,20 @@ export default function Index() {
                 </View>
                 <Image source={icons.bell} className="size-6"/>
             </View>
-        </View>
         <Search />
+        <View className="my-5">
+            <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                    Featured
+                </Text>
+                <TouchableOpacity>
+                    <Text className="text-base font-rubik-bold text-primary-300">
+                        See all
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+        </View>
     </SafeAreaView>
   );
 }
