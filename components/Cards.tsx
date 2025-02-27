f7cc99cd759e6e5ed34e162c5a12c3f843b0409a
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-02-27 17:40:03 -0500

    Add files via upload

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,23 +1,30 @@
-import {View, Text, TouchableOpacity, Image} from 'react-native'
-import images from "@/constants/images";
 import icons from "@/constants/icons";
-import React from "react";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Models } from "react-native-appwrite";
 
 interface Props {
+    item: Models.Document;
     onPress?: () => void;
 }
 
-
-export const FeaturedCard = ({ onPress } : Props) => {
+export const FeaturedCard = ({ item, onPress }: Props) => {
     return (
-        <TouchableOpacity onPress={onPress} className="flex flex-col items-start w-60 h-80 relative">
-            <Image source={images.japan} className="size-full rounded-2xl" />
-            <Image source={images.cardGradient} className="size-full rounded-2xl absolute bottom-0" />
+        <TouchableOpacity
+            onPress={onPress}
+            className="flex flex-col items-start w-60 h-80 relative"
+        >
+            <Image source={{ uri: item.image }} className="size-full rounded-2xl" />
+
+            <Image
+                source={images.cardGradient}
+                className="size-full rounded-2xl absolute bottom-0"
+            />
 
             <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
                 <Image source={icons.star} className="size-3.5" />
                 <Text className="text-xs font-rubik-bold text-primary-300 ml-1">
-                    4.4
+                    {item.rating}
                 </Text>
             </View>
 
@@ -26,24 +33,24 @@ export const FeaturedCard = ({ onPress } : Props) => {
                     className="text-xl font-rubik-extrabold text-white"
                     numberOfLines={1}
                 >
-                    Modern Apartment
+                    {item.name}
                 </Text>
                 <Text className="text-base font-rubik text-white" numberOfLines={1}>
-                    22W 15th St. New York
+                    {item.address}
                 </Text>
 
                 <View className="flex flex-row items-center justify-between w-full">
                     <Text className="text-xl font-rubik-extrabold text-white">
-                        $2,500
+                        ${item.price}
                     </Text>
                     <Image source={icons.heart} className="size-5" />
                 </View>
             </View>
         </TouchableOpacity>
-    )
-}
+    );
+};
 
-export const Card = ({  onPress }: Props) => {
+export const Card = ({ item, onPress }: Props) => {
     return (
         <TouchableOpacity
             className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative"
@@ -52,23 +59,23 @@ export const Card = ({  onPress }: Props) => {
             <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
                 <Image source={icons.star} className="size-2.5" />
                 <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
-                    4.9
+                    {item.rating}
                 </Text>
             </View>
 
-            <Image source={images.newYork } className="w-full h-40 rounded-lg" />
+            <Image source={{ uri: item.image }} className="w-full h-40 rounded-lg" />
 
             <View className="flex flex-col mt-2">
                 <Text className="text-base font-rubik-bold text-black-300">
-                    Cozy Studio
+                    {item.name}
                 </Text>
                 <Text className="text-xs font-rubik text-black-100">
-                    521 SW 53rd St. New York
+                    {item.address}
                 </Text>
 
                 <View className="flex flex-row items-center justify-between mt-2">
                     <Text className="text-base font-rubik-bold text-primary-300">
-                        $2,500
+                        ${item.price}
                     </Text>
                     <Image
                         source={icons.heart}
